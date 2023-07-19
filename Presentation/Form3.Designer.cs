26e483336f3abbfb1ad18128839fb30c332abdf4
Elaya Deerson <backtoharvest@hotmail.com>
2023-07-19 01:02:35 -0600

    Test

--- Presentation/Form3.Designer.cs
+++ Presentation/Form3.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            LbAKVersion = new Label();
+            LbAKVersionNumber = new Label();
+            LbAKCredits = new Label();
+            PicBoxDuck = new PictureBox();
+            BtnAKOk = new Button();
+            ((System.ComponentModel.ISupportInitialize)PicBoxDuck).BeginInit();
+            SuspendLayout();
+            // 
+            // LbAKVersion
+            // 
+            LbAKVersion.AutoSize = true;
+            LbAKVersion.Location = new Point(12, 9);
+            LbAKVersion.Name = "LbAKVersion";
+            LbAKVersion.Size = new Size(45, 15);
+            LbAKVersion.TabIndex = 0;
+            LbAKVersion.Text = "Version";
+            // 
+            // LbAKVersionNumber
+            // 
+            LbAKVersionNumber.AutoSize = true;
+            LbAKVersionNumber.Location = new Point(63, 9);
+            LbAKVersionNumber.Name = "LbAKVersionNumber";
+            LbAKVersionNumber.Size = new Size(57, 15);
+            LbAKVersionNumber.TabIndex = 1;
+            LbAKVersionNumber.Text = "(number)";
+            // 
+            // LbAKCredits
+            // 
+            LbAKCredits.AutoSize = true;
+            LbAKCredits.Location = new Point(12, 34);
+            LbAKCredits.Margin = new Padding(3, 10, 3, 0);
+            LbAKCredits.Name = "LbAKCredits";
+            LbAKCredits.Size = new Size(226, 165);
+            LbAKCredits.TabIndex = 2;
+            LbAKCredits.Text = "By Environment Cartography Department\r\n\r\nColaboration with:\r\nEin'x\r\nMangoFizz\r\n\r\n\r\n\r\n\r\n\r\nHalo CE Chimera SnowyMouse\r\n";
+            // 
+            // PicBoxDuck
+            // 
+            PicBoxDuck.Image = (Image)resources.GetObject("PicBoxDuck.Image");
+            PicBoxDuck.Location = new Point(294, 34);
+            PicBoxDuck.Name = "PicBoxDuck";
+            PicBoxDuck.Size = new Size(128, 128);
+            PicBoxDuck.TabIndex = 3;
+            PicBoxDuck.TabStop = false;
+            // 
+            // BtnAKOk
+            // 
+            BtnAKOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            BtnAKOk.FlatStyle = FlatStyle.Popup;
+            BtnAKOk.Location = new Point(326, 176);
+            BtnAKOk.Name = "BtnAKOk";
+            BtnAKOk.Size = new Size(75, 23);
+            BtnAKOk.TabIndex = 4;
+            BtnAKOk.Text = "Ok";
+            BtnAKOk.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(434, 211);
+            Controls.Add(BtnAKOk);
+            Controls.Add(PicBoxDuck);
+            Controls.Add(LbAKCredits);
+            Controls.Add(LbAKVersionNumber);
+            Controls.Add(LbAKVersion);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form3";
+            Text = "About Kraken";
+            ((System.ComponentModel.ISupportInitialize)PicBoxDuck).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LbAKVersion;
+        private Label LbAKVersionNumber;
+        private Label LbAKCredits;
+        private PictureBox PicBoxDuck;
+        private Button BtnAKOk;
     }
 }
---
+++ Presentation/Form4.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace chimera_editor.Presentation
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LbChimeraDown = new Label();
+            LbChimeraLink = new Label();
+            BtnChimeraOk = new Button();
+            SuspendLayout();
+            // 
+            // LbChimeraDown
+            // 
+            LbChimeraDown.AutoSize = true;
+            LbChimeraDown.Location = new Point(12, 9);
+            LbChimeraDown.Name = "LbChimeraDown";
+            LbChimeraDown.Size = new Size(249, 15);
+            LbChimeraDown.TabIndex = 0;
+            LbChimeraDown.Text = "Download the lastest version of Chimera here:";
+            // 
+            // LbChimeraLink
+            // 
+            LbChimeraLink.AutoSize = true;
+            LbChimeraLink.Location = new Point(12, 53);
+            LbChimeraLink.Name = "LbChimeraLink";
+            LbChimeraLink.Size = new Size(279, 15);
+            LbChimeraLink.TabIndex = 1;
+            LbChimeraLink.Text = "https://github.com/SnowyMouse/chimera/releases";
+            // 
+            // BtnChimeraOk
+            // 
+            BtnChimeraOk.FlatStyle = FlatStyle.Popup;
+            BtnChimeraOk.Location = new Point(206, 109);
+            BtnChimeraOk.Name = "BtnChimeraOk";
+            BtnChimeraOk.Size = new Size(75, 23);
+            BtnChimeraOk.TabIndex = 2;
+            BtnChimeraOk.Text = "Ok";
+            BtnChimeraOk.UseVisualStyleBackColor = true;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(480, 144);
+            Controls.Add(BtnChimeraOk);
+            Controls.Add(LbChimeraLink);
+            Controls.Add(LbChimeraDown);
+            Name = "Form4";
+            Text = "Chimera";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LbChimeraDown;
+        private Label LbChimeraLink;
+        private Button BtnChimeraOk;
+    }
+}
