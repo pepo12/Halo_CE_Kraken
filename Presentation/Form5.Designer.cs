e0036975c5bcb7b7034983f0900bd44bf04f10ac
Elaya Deerson <backtoharvest@hotmail.com>
2023-07-20 22:22:26 -0600

    test

---
+++ Presentation/Form5.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace chimera_editor.Presentation
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnTestCMFfaColor = new Button();
+            LbTestChtMsgFfaB = new Label();
+            TbxTestChtMsgFfaB = new TextBox();
+            LbTestChtMsgFfaG = new Label();
+            LbTestChtMsgFfaR = new Label();
+            TbxTestChtMsgFfa = new TextBox();
+            TbxTestChtMsgFfaR = new TextBox();
+            TbxTestChtMsgFfaA = new TextBox();
+            LbTestChtMsg = new Label();
+            LbTestChtMsgFfa = new Label();
+            PnlTestChtMsg = new Panel();
+            LbTestChtMsgFfaA = new Label();
+            SuspendLayout();
+            // 
+            // BtnTestCMFfaColor
+            // 
+            BtnTestCMFfaColor.FlatStyle = FlatStyle.Popup;
+            BtnTestCMFfaColor.Location = new Point(561, 45);
+            BtnTestCMFfaColor.Margin = new Padding(15, 3, 3, 3);
+            BtnTestCMFfaColor.Name = "BtnTestCMFfaColor";
+            BtnTestCMFfaColor.Size = new Size(24, 23);
+            BtnTestCMFfaColor.TabIndex = 572;
+            BtnTestCMFfaColor.UseVisualStyleBackColor = true;
+            // 
+            // LbTestChtMsgFfaB
+            // 
+            LbTestChtMsgFfaB.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            LbTestChtMsgFfaB.AutoSize = true;
+            LbTestChtMsgFfaB.Location = new Point(460, 49);
+            LbTestChtMsgFfaB.Margin = new Padding(0);
+            LbTestChtMsgFfaB.Name = "LbTestChtMsgFfaB";
+            LbTestChtMsgFfaB.Size = new Size(14, 15);
+            LbTestChtMsgFfaB.TabIndex = 571;
+            LbTestChtMsgFfaB.Text = "b";
+            LbTestChtMsgFfaB.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // TbxTestChtMsgFfaB
+            // 
+            TbxTestChtMsgFfaB.Location = new Point(474, 45);
+            TbxTestChtMsgFfaB.Margin = new Padding(0, 3, 0, 3);
+            TbxTestChtMsgFfaB.Name = "TbxTestChtMsgFfaB";
+            TbxTestChtMsgFfaB.Size = new Size(72, 23);
+            TbxTestChtMsgFfaB.TabIndex = 570;
+            // 
+            // LbTestChtMsgFfaG
+            // 
+            LbTestChtMsgFfaG.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            LbTestChtMsgFfaG.AutoSize = true;
+            LbTestChtMsgFfaG.Location = new Point(370, 49);
+            LbTestChtMsgFfaG.Margin = new Padding(0);
+            LbTestChtMsgFfaG.Name = "LbTestChtMsgFfaG";
+            LbTestChtMsgFfaG.Size = new Size(14, 15);
+            LbTestChtMsgFfaG.TabIndex = 569;
+            LbTestChtMsgFfaG.Text = "g";
+            LbTestChtMsgFfaG.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // LbTestChtMsgFfaR
+            // 
+            LbTestChtMsgFfaR.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            LbTestChtMsgFfaR.AutoSize = true;
+            LbTestChtMsgFfaR.Location = new Point(283, 49);
+            LbTestChtMsgFfaR.Margin = new Padding(0);
+            LbTestChtMsgFfaR.Name = "LbTestChtMsgFfaR";
+            LbTestChtMsgFfaR.Size = new Size(11, 15);
+            LbTestChtMsgFfaR.TabIndex = 568;
+            LbTestChtMsgFfaR.Text = "r";
+            LbTestChtMsgFfaR.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // TbxTestChtMsgFfa
+            // 
+            TbxTestChtMsgFfa.Location = new Point(384, 45);
+            TbxTestChtMsgFfa.Margin = new Padding(0, 3, 0, 3);
+            TbxTestChtMsgFfa.Name = "TbxTestChtMsgFfa";
+            TbxTestChtMsgFfa.Size = new Size(72, 23);
+            TbxTestChtMsgFfa.TabIndex = 567;
+            // 
+            // TbxTestChtMsgFfaR
+            // 
+            TbxTestChtMsgFfaR.Location = new Point(294, 45);
+            TbxTestChtMsgFfaR.Margin = new Padding(0, 3, 0, 3);
+            TbxTestChtMsgFfaR.Name = "TbxTestChtMsgFfaR";
+            TbxTestChtMsgFfaR.Size = new Size(72, 23);
+            TbxTestChtMsgFfaR.TabIndex = 566;
+            // 
+            // TbxTestChtMsgFfaA
+            // 
+            TbxTestChtMsgFfaA.Location = new Point(204, 45);
+            TbxTestChtMsgFfaA.Margin = new Padding(0, 3, 0, 3);
+            TbxTestChtMsgFfaA.Name = "TbxTestChtMsgFfaA";
+            TbxTestChtMsgFfaA.Size = new Size(72, 23);
+            TbxTestChtMsgFfaA.TabIndex = 565;
+            // 
+            // LbTestChtMsg
+            // 
+            LbTestChtMsg.AutoSize = true;
+            LbTestChtMsg.Location = new Point(17, 21);
+            LbTestChtMsg.Margin = new Padding(8, 12, 8, 0);
+            LbTestChtMsg.Name = "LbTestChtMsg";
+            LbTestChtMsg.Size = new Size(84, 15);
+            LbTestChtMsg.TabIndex = 564;
+            LbTestChtMsg.Text = "chat messages";
+            // 
+            // LbTestChtMsgFfa
+            // 
+            LbTestChtMsgFfa.AutoSize = true;
+            LbTestChtMsgFfa.BackColor = SystemColors.ControlLight;
+            LbTestChtMsgFfa.Location = new Point(37, 48);
+            LbTestChtMsgFfa.Margin = new Padding(8, 12, 8, 0);
+            LbTestChtMsgFfa.Name = "LbTestChtMsgFfa";
+            LbTestChtMsgFfa.Size = new Size(51, 15);
+            LbTestChtMsgFfa.TabIndex = 563;
+            LbTestChtMsgFfa.Text = "color ffa";
+            // 
+            // PnlTestChtMsg
+            // 
+            PnlTestChtMsg.BackColor = SystemColors.ControlLight;
+            PnlTestChtMsg.BorderStyle = BorderStyle.Fixed3D;
+            PnlTestChtMsg.Location = new Point(27, 45);
+            PnlTestChtMsg.Margin = new Padding(3, 38, 3, 3);
+            PnlTestChtMsg.Name = "PnlTestChtMsg";
+            PnlTestChtMsg.Size = new Size(139, 23);
+            PnlTestChtMsg.TabIndex = 562;
+            // 
+            // LbTestChtMsgFfaA
+            // 
+            LbTestChtMsgFfaA.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            LbTestChtMsgFfaA.AutoSize = true;
+            LbTestChtMsgFfaA.Location = new Point(191, 48);
+            LbTestChtMsgFfaA.Margin = new Padding(0);
+            LbTestChtMsgFfaA.Name = "LbTestChtMsgFfaA";
+            LbTestChtMsgFfaA.Size = new Size(13, 15);
+            LbTestChtMsgFfaA.TabIndex = 573;
+            LbTestChtMsgFfaA.Text = "a";
+            LbTestChtMsgFfaA.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(LbTestChtMsgFfaA);
+            Controls.Add(BtnTestCMFfaColor);
+            Controls.Add(LbTestChtMsgFfaB);
+            Controls.Add(TbxTestChtMsgFfaB);
+            Controls.Add(LbTestChtMsgFfaG);
+            Controls.Add(LbTestChtMsgFfaR);
+            Controls.Add(TbxTestChtMsgFfa);
+            Controls.Add(TbxTestChtMsgFfaR);
+            Controls.Add(TbxTestChtMsgFfaA);
+            Controls.Add(LbTestChtMsg);
+            Controls.Add(LbTestChtMsgFfa);
+            Controls.Add(PnlTestChtMsg);
+            Name = "Form5";
+            Text = "Form5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnTestCMFfaColor;
+        private Label LbTestChtMsgFfaB;
+        private TextBox TbxTestChtMsgFfaB;
+        private Label LbTestChtMsgFfaG;
+        private Label LbTestChtMsgFfaR;
+        private TextBox TbxTestChtMsgFfa;
+        private TextBox TbxTestChtMsgFfaR;
+        private TextBox TbxTestChtMsgFfaA;
+        private Label LbTestChtMsg;
+        private Label LbTestChtMsgFfa;
+        private Panel PnlTestChtMsg;
+        private Label LbTestChtMsgFfaA;
+    }
+}
